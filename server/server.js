6c42b4c2d3fc8999567e753bc4d68744b3542cb6
tintedsky <hongbonew@gmail.com>
2018-05-08 09:09:44 -0400

    broadcast event to others

--- server/server.js
+++ server/server.js
@@ -17,7 +17,12 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt:new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt:new Date().getTime()
